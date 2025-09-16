c2c76edaf8aeba7f98ec4a61b36f2402054831db
graziyye <156468604+graziyye@users.noreply.github.com>
2025-09-16 12:17:02 -0300

    js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-const sons = {'A': 'boom.wav', 'B': 'clap.wav', 'C': 'hihat.wav', 'D':'kick.wav', 'E':'openhat.wav','F':'ride.wav', 'G':'snare.wav', 'H':'tink.wav','I':'tom.wav'}
+//criacao das constantes do som
+
+const sons = {
+    'A': 'boom.wav',
+'B': 'clap.wav'
+, 'C': 'hihat.wav'
+, 'D':'kick.wav'
+, 'E':'openhat.wav'
+,'F':'ride.wav', 
+'G':'snare.wav', 
+'H':'tink.wav','I':'tom.wav'
+}
+//funcao para criar uma div pra cada tecla
+
+const criarDiv = (sons) =>{
+    div.classList('key');
+    div.textContent = texto;
+}
+
+exibir (sons);
+document.getElementById('container')
